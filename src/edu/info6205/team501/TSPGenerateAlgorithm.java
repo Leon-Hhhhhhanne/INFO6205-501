775514ec4337b43e89ac1e2278f9d2803da79d88
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2018-04-09 23:53:13 -0400

    Add Cross Over

--- src/edu/info6205/team501/TSPGenerateAlgorithm.java
+++ src/edu/info6205/team501/TSPGenerateAlgorithm.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.annotation.Generated;
@@ -15,13 +16,16 @@ import javax.annotation.Generated;
  */
 public class TSPGenerateAlgorithm {
 
+	private final static double P_DEFAULT_CROSS = 0.9;
+	private final static double P_DEFALUT_MUTATION = 0.1;
+
 	private ArrayList<ArrayList<Integer>> distanceList;
 	private ArrayList<Integer> xAxisList;
 	private ArrayList<Integer> yAxisList;
 	private ArrayList<ArrayList<Integer>> parentChromosomeList;
 	private ArrayList<ArrayList<Integer>> childChromosomeList;
 	private ArrayList<Integer> fitnessList;
-	private ArrayList<Integer> rouletteWheelList;
+	private double[] rouletteWheelList;
 	private int cityNum;
 	private int populationNum;
 	private Random random;
@@ -33,6 +37,7 @@ public class TSPGenerateAlgorithm {
 		this.populationNum = populationNum;
 	}
 
+	@SuppressWarnings("unchecked")
 	private void initDataFromTxtFile(String filename) throws Exception {
 		xAxisList = new ArrayList<>();
 		yAxisList = new ArrayList<>();
@@ -112,6 +117,7 @@ public class TSPGenerateAlgorithm {
 
 	// choose the best entity and put the best entity to children chromosome list
 	private void bestEntity() {
+		rouletteWheelList = new double[populationNum];
 		int shortestPath = fitnessList.get(0);
 		int shortestPosition = 0;
 		for (int i = 1; i < populationNum; i++)
@@ -121,28 +127,114 @@ public class TSPGenerateAlgorithm {
 			}
 		shortestDistance = shortestPath;
 		bestEntity = shortestPosition;
-		childChromosomeList.set(0,parentChromosomeList.get(shortestPosition));
+		childChromosomeList.set(0, parentChromosomeList.get(shortestPosition));
+
+		// set the roulette
+		double sum = 0;
+		for (int i = 0; i < populationNum; i++) {
+			sum += (double) 10 / fitnessList.get(i);
+		}
+		for (int i = 0; i < populationNum; i++) {
+			rouletteWheelList[i] = (double) 10 / fitnessList.get(i) / sum;
+		}
+		for (int i = 1; i < populationNum; i++) {
+			rouletteWheelList[i] += rouletteWheelList[i - 1];
+		}
 	}
 
 	// select and generate the remaining descendents
 	private void select() {
-        for (int i = 1; i < populationNum; i++) 
-        	childChromosomeList.add(parentChromosomeList.get(wheelOut((int) Math.random())));
-        
+		for (int i = 1; i < populationNum; i++)
+			childChromosomeList.set(i, parentChromosomeList.get(wheelOut(random.nextDouble())));
 	}
 
-    private int wheelOut(int ran) {
-        for (int i = 0; i < populationNum; i++) {
-            if (ran <= rouletteWheelList.get(i)) {
-                return i;
-            }
-        }
-        return 0;
-    }
-	
+	private int wheelOut(double ran) {
+		for (int i = 0; i < populationNum; i++) {
+			if (ran <= rouletteWheelList[i]) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	// mutation and crossover
+	private void evolution() {
+		for (int i = 0; i < populationNum; i += 2) {
+			double ranDouble = random.nextDouble();
+			if (ranDouble < P_DEFAULT_CROSS)
+				crossOver(i, i + 1);
+			else {
+				ranDouble = random.nextDouble();
+				if (ranDouble < P_DEFALUT_MUTATION)
+					mutation(i);
+				ranDouble = random.nextDouble();
+				if (ranDouble < P_DEFALUT_MUTATION) {
+					mutation(i + 1);
+				}
+			}
+		}
+	}
+
+	private void crossOver(int entity1, int entity2) {
+
+		int[] tempChromosomeList1 = new int[populationNum];
+		int[] tempChromosomeList2 = new int[populationNum];
+		int randomCity1 = Math.abs(random.nextInt() % populationNum);
+		int randomCity2 = Math.abs(random.nextInt() % populationNum);
+		while (randomCity1 == randomCity2)
+			randomCity2 = Math.abs(random.nextInt() % populationNum);
+		if (randomCity1 > randomCity2) {
+			int temp = randomCity1;
+			randomCity1 = randomCity2;
+			randomCity2 = temp;
+		}
+
+		// Move some chromosome to temp chromosome list 2
+		int i, j, k;
+		for (i = 0, j = randomCity2; j < populationNum; i++, j++)
+			tempChromosomeList2[i] = childChromosomeList.get(entity1).get(j);
+
+		int startLocation = i;
+		for (k = 0, j = startLocation; j < populationNum;) {
+			tempChromosomeList2[j] = childChromosomeList.get(entity2).get(k++);
+			for (i = 0; i < startLocation; i++)
+				if (tempChromosomeList2[i] == tempChromosomeList2[j])
+					break;
+			if (i == startLocation)
+				j++;
+		}
+
+		startLocation = randomCity1;
+		for (k = 0, j = 0; k < populationNum;) {
+			tempChromosomeList1[j] = childChromosomeList.get(entity1).get(k++);
+			for (i = 0; i < startLocation; i++)
+				if (tempChromosomeList1[j] == childChromosomeList.get(entity2).get(i))
+					break;
+			if (i == startLocation)
+				j++;
+		}
+
+		startLocation = populationNum - randomCity1;
+		for (i = 0, j = startLocation; j < populationNum; j++, i++)
+			tempChromosomeList1[j] = childChromosomeList.get(entity2).get(i);
+
+		for (i = 0; i < populationNum; i++) {
+			childChromosomeList.get(entity1).set(i, tempChromosomeList1[i]);
+			childChromosomeList.get(entity2).set(i, tempChromosomeList2[i]);
+		}
+	}
+
+	private void mutation(int target) {
+
+	}
+
 	private void generate() {
 		generationCounter++;
 		bestEntity();
+		select();
+		System.out.println(childChromosomeList);
+		evolution();
+		System.out.println(childChromosomeList);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -171,6 +263,6 @@ public class TSPGenerateAlgorithm {
 		tspGenerateAlgorithm.generate();
 		// System.out.println(tspGenerateAlgorithm.bestEntity);
 		// System.out.println(tspGenerateAlgorithm.shortestDistance);
-
+		// System.out.println(tspGenerateAlgorithm.childChromosomeList);
 	}
 }
